234167dbedd8b13711bce127c6b50a257cc2c41f
chishankar <chishankar@gmail.com>
2017-09-09 11:58:11 -0400

    My changes

---
+++ public/scripts/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var App = require('./App.jsx');
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
