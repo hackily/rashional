f9dbaf3660cfe57b413d5124122731719a23c46e
chishankar <chishankar@gmail.com>
2017-09-09 13:22:09 -0400

    For Sai

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,20 +1,26 @@
 const React = require('react');
-
+const Pic = require('./pic.jsx')
 
 class App extends React.Component {
-  constructor () {
+  constructor() {
     super()
-    this.state = { n: 0 }
+    this.state = {
+      n: 0
+    }
   }
-  render () {
+  render() {
     return <div>
-      <h1>clicked {this.state.n} times</h1>
+      <h1>clicked {this.state.n}
+        times</h1>
       <button onClick={this.handleClick.bind(this)}>click me!</button>
+      <Pic/>
     </div>
   }
-  handleClick () {
-    this.setState({ n: this.state.n + 1 })
+  handleClick() {
+    this.setState({
+      n: this.state.n + 1
+    })
   }
 }
 
-module.exports = App;
+module.exports = App
