0920e4b5529d67573bcea4149cd7c62f0b70f2cf
chishankar <chishankar@gmail.com>
2017-09-09 15:28:22 -0400

    Update

--- frontend/components/pic.jsx
+++ frontend/components/pic.jsx
@@ -1,13 +1,21 @@
 const React = require('react');
+// const boxStyle = require('./pic.css')
+import './pic.css'
 
 class Pic extends React.Component {
   constructor() {
     super()
   }
+  // const divStyle = {
+  //   color: 'blue'
+  // };
+
   render() {
-    return <div>
-      Helloasdfasdfasdf
-    </div>
+    return(
+    <div> Hello, Welcome to BitCamp!
+       <div color="blue" width="100px" height="100px" border-style="solid">This is a test</div>
+    </div>);
+
   }
 
 }
