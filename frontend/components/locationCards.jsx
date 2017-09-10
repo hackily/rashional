2462eeb0103f07b8dc9e87b54845c400723726ee
chishankar <chishankar@gmail.com>
2017-09-09 23:51:04 -0400

    Added location.rjx

---
+++ frontend/components/locationCards.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+const locationCards = require('../node_modules/materialize-css')
+const locationCards_1 = require('../node_modules/materialize.min.js')
+
+class Location exent React.Component{
+  constructor(){
+    super()
+  }
+
+  render(){
+    return(
+      
+
+
+
+    );
+  }
+}
