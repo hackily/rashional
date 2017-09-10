d6d74072efcec05e2e80ea7aa8a4f9d69ecf70f3
Wang, Neil <Neil.Wang@capitalone.com>
2017-09-09 23:29:44 -0400

    Cleanup

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,23 +3,42 @@ const Pic = require('./pic.jsx');
 const bootstrap = require('./css/bootstrap.min.css')
 const bootstrap_1 = require('./css/bootstrap.css')
 const titleStyle = require('./main.css')
+const Message = require('./message.jsx')
+const RashChart = require('./rashChart.jsx')
 
 class App extends React.Component {
   constructor() {
     super()
+    this.state = {data: [
+      {x: 'A', y: 20},
+      {x: 'B', y: 30},
+      {x: 'C', y: 40},
+      {x: 'D', y: 20},
+      {x: 'E', y: 40},
+      {x: 'F', y: 25},
+      {x: 'G', y: 5}
+      ]
+    }
   }
-  render() {
-    return (
-      <div>
-        <div className="container">
-          <div className="jumbotron">
-            <h1>R<i>#</i>iOnal</h1>
-          </div>
+  render () {
+    return <div>
+      <div className="container">
+        <div className="jumbotron">
+          <h1>R<i>#</i>iOnal</h1>
         </div>
         <Pic/>
       </div>
-    );
+      <h1>clicked {this.state.n} times</h1>
+      <button onClick={this.handleClick.bind(this)}>click me!</button>
+      <Message/>
+      <RashChart ref="rashChart" data = {this.state.data} />
+    </div>
 
+  }
+  handleClick () {
+    //this.setState({ n: this.state.n + 1 })
+    this.state.data[0].y = this.state.data[0].y + 50;
+    this.refs.rashChart.updateChart();
   }
 
 }
